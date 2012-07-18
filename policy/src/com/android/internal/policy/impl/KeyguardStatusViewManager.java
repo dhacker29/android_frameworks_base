75298748ffb3aeb9f17f95cc7288e4a36d866dd7
Anju Mathapati <anjucm@codeaurora.org>
2012-07-18 16:41:34 +0530

    Telephony: Handle ICC IO error & display PIN/PUK count
    frameworks/base: Handle ICC IO error & display PIN/PUK count.
    
    Following changes have been made as part of this:
    -> At present in Android all ICC Card states other than ICC
       PRESENT are treated as ICC ABSENT. Adding functionality
       to handle ICC IO error card state.
    -> Changes done to display retry counter on wrong entry of
       PIN1,and message to indicate Code accepted/PIN1 blocked
       during PIN1 verification as per certain carrier requirements.
    ->The current APIs that are used to verify the PIN and PUK only convey
      whether the operation succeeded or failed. As a result on ANY failure
      clients ask the user to re-enter the PIN.
      Add 2 new APIs that report the actual error code in case of failure.
    
    Change-Id: I6a3d459ab96eb351165e15f7fa47280328396f57
    CRs-Fixed: 184479, 260481, 181402, 181380, 181401
    
    Conflicts:
    	core/res/res/values/public.xml

--- policy/src/com/android/internal/policy/impl/KeyguardStatusViewManager.java
+++ policy/src/com/android/internal/policy/impl/KeyguardStatusViewManager.java
@@ -911,6 +911,8 @@ class KeyguardStatusViewManager implements OnClickListener {
                 return StatusMode.SimPermDisabled;
             case UNKNOWN:
                 return StatusMode.SimMissing;
+            case CARD_IO_ERROR:
+                return StatusMode.SimIOError;
         }
         return StatusMode.SimMissing;
     }
@@ -987,6 +989,12 @@ class KeyguardStatusViewManager implements OnClickListener {
                     mEmergencyButtonEnabledBecauseSimLocked = true;
                 }
                 break;
+
+            case SimIOError:
+                carrierText = makeCarierString(mPlmn,
+                        getContext().getText(R.string.lockscreen_sim_error_message_short));
+                 mEmergencyButtonEnabledBecauseSimLocked = true;
+                break;
         }
 
         Log.d(TAG, "updateCarrierStateWithSimStatus(): Sim Status = " + mStatus
@@ -1051,7 +1059,12 @@ class KeyguardStatusViewManager implements OnClickListener {
         /**
          * The sim card is permanently disabled due to puk unlock failure
          */
-        SimPermDisabled(false);
+        SimPermDisabled(false),
+
+        /**
+         * The sim card is faulty
+         */
+        SimIOError(true);
 
         private final boolean mShowStatusLines;
 
